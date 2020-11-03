bbce2b623d1d33482728dac57c29182b492c67cf
KhacTrieu <khtrieu1102@gmail.com>
2020-11-03 17:06:20 +0700

    add course item card

---
+++ src/components/cores/course/course-item-card.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+const CourseItemCard = (props) => {
+	const { courseDetails } = props;
+	const {
+		title,
+		author,
+		level,
+		released,
+		duration,
+		description,
+	} = courseDetails;
+
+	const handlePress = () => {
+		console.log("item pressed");
+	};
+
+	return (
+		<TouchableOpacity
+			style={{
+				marginRight: 20,
+				height: 220,
+				width: 220,
+				backgroundColor: "#ebf2ff",
+				shadowColor: "#000",
+				shadowOffset: {
+					width: 5,
+					height: 10,
+				},
+				shadowOpacity: 0.23,
+				shadowRadius: 2.62,
+			}}
+			onPress={handlePress}
+		>
+			<Image
+				source={{
+					uri:
+						"https://cdn.yankodesign.com/images/design_news/2018/10/the-keyboard-like-youve-never-seen-it/fangyuan_mechanical_keyboard_layout.jpg",
+				}}
+				style={{ height: 100 }}
+			/>
+			<View style={{ marginTop: 10, marginLeft: 10 }}>
+				<Text style={{ fontWeight: "bold" }}>{title}</Text>
+				<Text>{author}</Text>
+				<Text>
+					{level} - {released} - {duration}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+export default CourseItemCard;
