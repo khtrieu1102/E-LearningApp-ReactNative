59744ec05340f074c8ff6cabb2f7e4ef512b6441
KhacTrieu <khtrieu1102@gmail.com>
2020-11-12 14:34:09 +0700

    add very simple author tag, just button and his name

---
+++ src/components/cores/author/author-simple.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native"
+
+const AuthorSimpleItem = ({ authorName }) => {
+    const navigation = useNavigation();
+
+	const handlePress = () => {
+		console.log("Go to author name");
+		// navigation.navigate("AuthorDetail", { authorDetails: authorDetails });
+	};
+
+	return (
+		<TouchableOpacity
+			style={{
+				backgroundColor: "#dedede",
+				flexDirection: "column",
+				borderRadius: 20,
+				marginRight: 5,
+			}}
+			onPress={handlePress}
+		>
+			<Text
+				style={{
+					flex: 1,
+					color: "black",
+					alignSelf: "center",
+					fontSize: 12,
+					paddingTop: 5,
+					paddingBottom: 5,
+					paddingLeft: 10,
+					paddingRight: 10,
+				}}
+			>
+				by {authorName}
+			</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default AuthorSimpleItem;
