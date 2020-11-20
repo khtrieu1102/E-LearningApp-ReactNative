ecb0213d762972cc83b7c148871f06a7d524af50
KhacTrieu <khtrieu1102@gmail.com>
2020-11-20 16:47:32 +0700

    Hard code to show 2 stack screens

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-
-import Login from "./src/components/authorization/log-in/log-in.view"
-import Register from "./src/components/authorization/register/register.view"
-import ForgotPassword from "./src/components/authorization/forgot-password/forgot-password.view"
-import AuthorizationHome from "./src/components/authorization/authorization-home/authorization-home.view"
-import Home from "./src/components/main/home/home.view"
-import FavoriteCourses from "./src/components/main/favorite-courses/favorite-courses.view"
-
 import AuthorizationStack from "./src/components/authorization/authorizationStack"
+import MainTabNavigation from "./src/components/main/mainNavigation"
+import Logo from "./src/components/authorization/assets/fit-hcmus-logo.png"
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <View style={styles.container}>
+//         <MainTabNavigation />
+//       </View>
+//     </NavigationContainer>
+//   );
+// }
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
+    <NavigationContainer style={{backgroundColor: "white"}}>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
         <AuthorizationStack />
       </View>
     </NavigationContainer>
