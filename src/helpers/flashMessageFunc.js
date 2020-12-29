f41821f5ac8e6654d07f3181109cbae6e7b13f9e
KhacTrieu <khtrieu1102@gmail.com>
2020-12-29 18:55:09 +0700

    split helper functions, global message

---
+++ src/helpers/flashMessageFunc.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { showMessage, hideMessage } from "react-native-flash-message";
+import { useDispatch } from "react-redux";
+
+const CustomMessageTemplate = ({ messageToShow }) => {
+    const dispatch = useDispatch();
+    
+    return <span style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: 0 }}>
+        <span style={{color: "white"}}>{messageToShow}</span>
+        <button 
+            style={{ border: "none", borderRadius: 20 }}
+            onClick={() => { 
+                dispatch({type: "RESET_ERROR_MESSAGE"}); 
+                hideMessage();} 
+            }>
+            Dismiss
+        </button>
+    </span>
+}
+
+const showGlobalError = (value) => {
+	const messageToShow = (typeof value == "string") ? value : "Something's wrong! Try again!";
+    showMessage({
+		hideOnPress: false,
+		description: <CustomMessageTemplate messageToShow={messageToShow} />,
+		message: "ERROR!",
+		type: "danger",
+		icon: "danger",
+	});
+}
+
+const showGlobalInfo = (value) => {
+	if (typeof value == "string")
+		showMessage({
+			autoHide: true,
+			duration: 2000,
+			onPress: () => {
+				console.log("Close!");
+			},
+			message: "IT'S COOL!",
+			description: 
+				<span style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: 0 }}>
+					<span style={{color: "white"}}>{value}</span>
+				</span>,
+			// description: "Something went wrong",
+			type: "info",
+			icon: "info",
+		});
+}
+
+export default { showGlobalError, showGlobalInfo };
